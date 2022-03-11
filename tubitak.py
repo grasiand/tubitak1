271b772c2e43cf6b4bafe0477f7ab7fee64ffea6
picadoor1 <87449297+picadoor1@users.noreply.github.com>
2022-03-11 09:27:21 +0300

    Add files via upload

--- tubitak.py
+++ tubitak.py
@@ -1,186 +1,147 @@
 import os
-import colorama
-from colorama import Fore, Back, Style
 author="Grasiand"
-print(Fore.GREEN)
 print("""
-1)Binary Nedir?
-2)Binary Kodları Nasıl Çalışır?
-3)Harf'in Binary Kodunu Görmek İçin Bir Harf'e Basınız.
-4)Temizle
+aa)Binary Nedir?
+bb)Binary Kodları Nasıl Çalışır?
+cc)Temizle
+Binary Kodunu Görmek İçin Klavye'den Bir Tuşa Basınız.
 """)
-print(Style.RESET_ALL)
 vur = input("Lütfen yapacağınızı seçiniz -> ")
+os.system("color")
 
-if vur == "1":
-   print(Fore.CYAN)
+if vur == "aa":
    print("İkili Kod (Binary), Gottfried Leibniz tarafından icat edilen ve yalnızca iki sayıdan oluşan bir sayı sistemidir. ")
-   print(Style.RESET_ALL)
 
-if vur == "2":
-   print(Fore.RED)
+if vur == "bb":
+
    print("0’lar ve 1’ler sırasıyla KAPALI veya AÇIK’ı temsil eder. Aslında Matematik’teki mantık konusunu örnek verebiliriz. Bir transistörde “0” elektrik akışı olmadığını, “1” elektriğin akmasına izin verildiğini gösterir. Bu şekilde, sayılar hesaplamaya izin verecek şekilde hesaplama cihazının içinde fiziksel olarak temsil edilir. Totoloji ve çelişki mantığıyla oldukça benzer olduğunu söyleyebiliriz.")
-   print(Style.RESET_ALL)
-
 
 if vur == "A":
-   print(Fore.MAGENTA)
    print("A harfinin Binary kodu: 01000001")
-   print(Style.RESET_ALL)
-
+   
 if vur == "B":
-   print(Fore.MAGENTA)
    print("B harfinin Binary kodu: 01000010")
-   print(Style.RESET_ALL)
 
 if vur == "C":
-   print(Fore.MAGENTA)
    print("C harfinin Binary kodu: 01000011")
-   print(Style.RESET_ALL)
 
 if vur == "Ç":
-   print(Fore.MAGENTA)
    print("Ç harfinin Binary kodu: 11000011 10000111")
-   print(Style.RESET_ALL)
 
 if vur == "D":
-   print(Fore.MAGENTA)
    print("D harfinin Binary kodu: 01000100")
-   print(Style.RESET_ALL)
 
 if vur == "E":
-   print(Fore.MAGENTA)
    print("E harfinin Binary kodu: 01000101")
-   print(Style.RESET_ALL)
 
 if vur == "F":
-   print(Fore.MAGENTA)
    print("F harfinin Binary kodu: 01000110")
-   print(Style.RESET_ALL)
 
 if vur == "G":
-   print(Fore.MAGENTA)
    print("G harfinin Binary kodu: 01000111")
-   print(Style.RESET_ALL)
-
+   
 if vur == "Ğ":
-   print(Fore.MAGENTA)
    print("Ğ harfinin Binary kodu: 11000100 10011110")
-   print(Style.RESET_ALL)
 
 if vur == "H":
-   print(Fore.MAGENTA)
    print("H harfinin Binary kodu: 01001000")
-   print(Style.RESET_ALL)
 
 if vur == "İ":
-   print(Fore.MAGENTA)
    print("İ harfinin Binary kodu: 11000100 10110000")
-   print(Style.RESET_ALL)
 
 if vur == "I":
-   print(Fore.MAGENTA)
    print("I harfinin Binary kodu: 01001001")
-   print(Style.RESET_ALL)
 
 if vur == "J":
-   print(Fore.MAGENTA)
    print("J harfinin Binary kodu: 01001010")
-   print(Style.RESET_ALL)
 
 if vur == "K":
-   print(Fore.MAGENTA)
    print("K harfinin Binary kodu: 01001011")
-   print(Style.RESET_ALL)
 
 if vur == "L":
-   print(Fore.MAGENTA)
    print("L harfinin Binary kodu: 01001100")
-   print(Style.RESET_ALL)
 
 if vur == "M":
-   print(Fore.MAGENTA)
    print("M harfinin Binary kodu: 01001101")
-   print(Style.RESET_ALL)
 
 if vur == "N":
-   print(Fore.MAGENTA)
    print("N harfinin Binary kodu: 01001110")
-   print(Style.RESET_ALL)
-
+   
 if vur == "O":
-   print(Fore.MAGENTA)
    print("O harfinin Binary kodu: 01001111")
-   print(Style.RESET_ALL)
 
 if vur == "Ö":
-   print(Fore.MAGENTA)
    print("Ö harfinin Binary kodu: 11000011 10010110")
-   print(Style.RESET_ALL)
 
 if vur == "P":
-   print(Fore.MAGENTA)
    print("P harfinin Binary kodu: 01010000")
-   print(Style.RESET_ALL)
 
 if vur == "R":
-   print(Fore.MAGENTA)
    print("R harfinin Binary kodu: 01010010")
-   print(Style.RESET_ALL)
 
 if vur == "S":
-   print(Fore.MAGENTA)
    print("N harfinin Binary kodu: 01010011")
-   print(Style.RESET_ALL)
 
 if vur == "Ş":
-   print(Fore.MAGENTA)
    print("Ş harfinin Binary kodu: 11000101 10011110")
-   print(Style.RESET_ALL)
 
 if vur == "T":
-   print(Fore.MAGENTA)
    print("T harfinin Binary kodu: 01010100")
-   print(Style.RESET_ALL)
 
 if vur == "U":
-   print(Fore.MAGENTA)
    print("U harfinin Binary kodu: 01010101")
-   print(Style.RESET_ALL)
 
 if vur == "Ü":
-   print(Fore.MAGENTA)
    print("Ü harfinin Binary kodu: 11000011 10011100")
-   print(Style.RESET_ALL)
 
 if vur == "V":
-   print(Fore.MAGENTA)
    print("V harfinin Binary kodu: 01010110")
-   print(Style.RESET_ALL)
 
 if vur == "Y":
-   print(Fore.MAGENTA)
    print("Y harfinin Binary kodu: 01011001")
-   print(Style.RESET_ALL)
 
 if vur == "Z":
-   print(Fore.MAGENTA)
    print("Z harfinin Binary kodu: 01011010")
-   print(Style.RESET_ALL)
 
 if vur == "X":
-   print(Fore.MAGENTA)
    print("X harfinin Binary kodu: 01011000")
-   print(Style.RESET_ALL)
 
 if vur == "Q":
-    print(Fore.MAGENTA)
     print("Q harfinin Binary Kodu: 01010001")
-    print(style.RESET_ALL)
 
+if vur == "1":
+    print("1 sayısın Binary Kodu: 00110001")
+
+if vur == "2":
+    print("2 sayısın Binary Kodu: 00110010")
+
+if vur == "3":
+    print("3 sayısın Binary Kodu: 00110011")
+    
 if vur == "4":
-    os.system("clear")
-    os.system("python3 tubitak.py")
+    print("4 sayısın Binary Kodu: 00110100")
+
+if vur == "5":   
+    print("5 sayısın Binary Kodu: 00110101")
+    
+if vur == "6":
+    print("6 sayısın Binary Kodu: 00110110")
+    
+if vur == "7":
+    print("7 sayısın Binary Kodu: 00110111")
+
+if vur == "8":
+    print("8 sayısının Binary Kodu: 00111000")
+
+if vur == "9":
+    print("9 sayısın Binary Kodu: 00111001")
+
+if vur == "0":
+    print("0 sayısın Binary Kodu: 00110000")
+    
+if vur == "cc":
+    os.system("cls")
+    os.system("tubitak.py")
 
 else :
-    os.system("python3 tubitak.py")
+    os.system("tubitak.py")
